bf8d070cdbea363294cc2debbbc25c753b8a8961
Juan C. Müller <jcmuller@gmail.com>
2018-03-15 10:20:19 -0400

    Accept multiple arguments

--- main.go
+++ main.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"os"
+	"strings"
 
 	"github.com/jcmuller/choosy/chooser"
 )
 
 func main() {
-	if len(os.Args) != 2 {
+	if len(os.Args) < 2 {
 		os.Exit(0)
 	}
 
-	arg := os.Args[1]
+	arg := strings.Join(os.Args[1:], " ")
 
 	c := chooser.New(arg)
 	c.Call()
